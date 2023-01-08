5db32d084d3bc5ce8d82bde86831e1759c46be6c
jukuly <vpare070@uottawa.ca>
2023-01-08 02:31:14 -0500

    hero section done

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,32 @@
+import './home.scss';
+
 export default function() {
   return (
-    <div>Home</div>
+    <main>
+      <div className='hero'>
+        <h1 className='title'>
+          <span className='gradient-text'>ScribeAI.</span>
+          <br />
+          <span className='description'>The AI powered writing assistant.</span>
+        </h1>
+        <div className='show-off'>
+          <div className='text-editor'>
+            <div className='menu'>
+              <span className='material-symbols-outlined minimize'>minimize</span>
+              <span className='material-symbols-outlined close'>close</span>
+            </div>
+            <p className='text'>
+              Hello professor,<br />
+              <br />
+              <span className='selected'>I was wondering if after next class, I could come to your office to talk about the last exam. There</span>
+            </p>
+            <div className='pop-up'>
+              <img src={require('../../assets/show-off-pop-up.png')} alt='show-off-pop-up' draggable='false' />
+              <span className='material-symbols-outlined close pointer'>near_me</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
