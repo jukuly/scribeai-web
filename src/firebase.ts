d207466513305474580086f123151841f763ca1b
jukuly <vpare070@uottawa.ca>
2023-02-11 18:11:03 -0500

    firebase production

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,6 +5,7 @@ import { getAuth } from "firebase/auth";
 
 const app = initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
